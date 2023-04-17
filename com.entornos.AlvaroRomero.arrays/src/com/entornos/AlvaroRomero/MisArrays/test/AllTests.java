fda5d1c20119a062f2e608bc5278c17c3dcdf902
Nuzagama <alvarorc25@gmail.com>
2023-04-17 12:54:39 +0200

    Quinto Ejercicio Suit

---
+++ com.entornos.AlvaroRomero.arrays/src/com/entornos/AlvaroRomero/MisArrays/test/AllTests.java
@@ -0,0 +1,10 @@
+package com.entornos.AlvaroRomero.MisArrays.test;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ MisTest.class })
+public class AllTests {
+
+}
